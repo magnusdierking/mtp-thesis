ae3839052c3bce56636fbcbbac17a7a7324712ae
Joao Carvalho <joao@robot-learning.de>
2025-09-04 15:06:56 +0200

    Fix paths.

--- hydrax/algs/mtp/mtp.py
+++ hydrax/algs/mtp/mtp.py
@@ -195,8 +195,8 @@ class MTP(SamplingBasedController):
                 mtp_controls_full = mtp_controls_full.at[:, interp_len:].set(tail) # (B, T, U)
 
             elif self.interpolation == 'bspline':
-                mtp_controls_full = jnp.einsum("bmd,hm->bhd", control_points, self.bmat)
-            
+                # TODO this has to incorporate the last taken control as a first point + interpolate
+                mtp_controls_full = jnp.einsum("...md,hm->...hd", control_points, self.bmat)
             elif self.interpolation == 'linear':
                 # interp_vals: (B, interp_len, U)
                 interp_vals = jax.vmap(interpolate_path, in_axes=(0, None))(control_points, num_interp)
