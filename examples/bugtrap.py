046db509b82261ea70c50105bae2264f452faa03
magnusdierking <mag.dierking@web.de>
2025-10-23 16:22:30 +0200

    added custom CEM for experiments

--- examples/bugtrap.py
+++ examples/bugtrap.py
@@ -1,16 +1,12 @@
 import argparse
-import os
-# os.environ["JAX_LOG_COMPILES"] = "1"
-import evosax
-import evosax.algorithms
-import mujoco
 
 from hydrax.algs import MTP
 from hydrax.algs import AnMTP
 from hydrax.algs import MPPI, Evosax, PredictiveSampling
+from hydrax.algs import CEM
 from hydrax.risk import WorstCase
 from hydrax.simulation.deterministic import run_interactive
-from hydrax.simulation.deterministic_experiment import run_headless_simulation
+
 from hydrax.tasks.bugtrap import BugTrap
 
 
@@ -30,15 +26,8 @@ parser = argparse.ArgumentParser(
 subparsers = parser.add_subparsers(
     dest="algorithm", help="Sampling algorithm (choose one)"
 )
-subparsers.add_parser("ps", help="Predictive Sampling")
 subparsers.add_parser("mppi", help="Model Predictive Path Integral Control")
-subparsers.add_parser("cmaes", help="CMA-ES")
-subparsers.add_parser(
-    "samr", help="Genetic Algorithm with Self-Adaptation Mutation Rate (SAMR)"
-)
-subparsers.add_parser("de", help="Differential Evolution")
-subparsers.add_parser("gld", help="Gradient-Less Descent")
-subparsers.add_parser("rs", help="Uniform Random Search")
+subparsers.add_parser("cem", help="CEM")
 subparsers.add_parser("mtp", help="MTP")
 subparsers.add_parser("anmtp", help="Annealed MTP")
 args = parser.parse_args()
@@ -46,53 +35,43 @@ args = parser.parse_args()
 # Set the controller based on command-line arguments
 if args.algorithm is None: 
     args.algorithm = "mtp"  # Default to MTP
-if args.algorithm == "ps":
-    print("Running predictive sampling")
-    ctrl = PredictiveSampling(
-        task,
-        num_samples=16,
-        noise_level=0.1,
-        num_randomizations=10,
-        risk_strategy=WorstCase(),
-    )
 
 elif args.algorithm == "mppi":
     print("Running MPPI")
-    ctrl = MPPI(task, num_samples=32, noise_level=0.6, temperature=0.01)
-    save_path = "./../data/headless_bugtrap_mppi"
+    ctrl = MPPI(task, 
+                num_samples=128, 
+                noise_level=0.35, 
+                temperature=0.01)
+    # save_path = "./../data/headless_bugtrap_mppi"
+    save_path = "./../data/test"
 
-elif args.algorithm == "cmaes":
-    print("Running CMA-ES")
-    ctrl = Evosax(task, evosax.algorithms.Sep_CMA_ES, num_samples=16)
-
-elif args.algorithm == "samr":
-    print("Running genetic algorithm with Self-Adaptation Mutation Rate (SAMR)")
-    ctrl = Evosax(task, evosax.algorithms.SAMR_GA, num_samples=16)
-
-elif args.algorithm == "de":
-    print("Running Differential Evolution (DE)")
-    ctrl = Evosax(task, evosax.algorithms.DifferentialEvolution, num_samples=16)
-
-elif args.algorithm == "gld":
-    print("Running Gradient-Less Descent (GLD)")
-    ctrl = Evosax(task, evosax.algorithms.GradientlessDescent, num_samples=16)
+elif args.algorithm == "cem":
+    print("Running CEM")
+    ctrl = CEM(
+        task,
+        num_samples=128,
+        num_elites=8,
+        sigma_start=0.2,
+        sigma_min=0.05,
+        alpha=0.0,
+    )
+    save_path = "./../data/headless_bugtrap_cem"
+    
 
 elif args.algorithm == "mtp":
     print("Running MTP")
     seed = 0
     ctrl = MTP(
             task,
-            num_samples=64,
-            M=2,
-            N=16,
-            sigma_min=0.2,
-            sigma_start=0.4,
-            sigma_max=0.4,
-            temperature=0.1,
-            num_elites=2,
-            beta=0.5,
-            alpha=0.05,
-            interpolation='bspline',
+            num_samples=128,
+            M=4,
+            N=32,
+            sigma_start=0.15, # 0.2
+            temperature=0.01,
+            num_elites=8, # 2
+            beta=0.55, # 0.3
+            alpha=0.0,
+            interpolation='akima',
             num_randomizations=1,
             seed=seed,
         )
@@ -108,29 +87,19 @@ elif args.algorithm == "anmtp":
             N=32,
             sigma_min=0.2,
             num_elites=2,
-            beta=0.25,
-            alpha=0.05,
+            beta=0.85,
+            alpha=0.25,
             interpolation='bspline',
             num_randomizations=1,
             seed=seed,
         )
     save_path = "./../data/headless_bugtrap_anmtp"
     
-elif args.algorithm == "rs":
-    print("Running uniform random search")
-    # es_params = evosax.strategies.random.EvoParams(
-    #     range_min=-1.0,
-    #     range_max=1.0,
-    # )
-    ctrl = Evosax(
-        task, evosax.algorithms.RandomSearch, num_samples=16
-    )
 else:
     parser.error("Invalid algorithm")
 
 # Define the model used for simulation
-mj_model = task.mj_model
-mj_data = mujoco.MjData(mj_model)
+mj_model, mj_data = task.reset()
 
 # Run the interactive simulation
 run_interactive(
@@ -142,16 +111,4 @@ run_interactive(
     trace_width=0.25,
     max_traces=25,
     record_video=True,
-)
-exit()
-# run headless simulation
-seeds = [0, 1, 2, 3, 4, 5, 6]
-run_headless_simulation(
-    task,
-    ctrl,
-    seeds=seeds,
-    frequency=25,
-    max_step=500,       
-    log_file_prefix="bugtrap",
-    save_path=save_path,
 )
--- examples/pusht_franka.py
+++ examples/pusht_franka.py
@@ -1,6 +1,6 @@
 import argparse
 
-from hydrax.algs import MPPI, MTP
+from hydrax.algs import MPPI, MTP, CEM
 # from hydrax.algs.mtp.an_mtp_opt import AnMTP
 from hydrax.algs.mtp.an_mtp_dr import AnMTP
 
@@ -25,11 +25,12 @@ subparsers = parser.add_subparsers(
     dest="algorithm", help="Sampling algorithm (choose one)"
 )
 subparsers.add_parser("mppi", help="Model Predictive Path Integral Control")
+subparsers.add_parser("cem", help="Cross Entropy Method")
 subparsers.add_parser("mtp", help="MTP")
 subparsers.add_parser("anmtp", help="Annealed MTP")
 args = parser.parse_args()
 
-seed = 36
+seed = 43 # 36, ... 
 
 # Set the controller based on command-line arguments
 if args.algorithm is None: 
@@ -38,74 +39,96 @@ elif args.algorithm == "mppi":
     print("Running MPPI")
     ctrl = MPPI(
         task,
-        num_samples=64,
+        num_samples=128,
         noise_level=0.3,
         temperature=0.1,
-        num_randomizations=2,
-        colorize_noise=True,   # !experimental
-        seed=seed,
+        num_randomizations=1,
+        colorize_noise=False,   # !experimental
+        alpha=0.1,
+        seed=seed
     )
+    error_log = "./../data/error_log_pushT/mppi_{seed}.npy".format(seed=seed)
+    
+elif args.algorithm == "cem":
+    print("Running CEM")
+    ctrl = CEM(
+        task,
+        num_samples=128,
+        num_elites=12,
+        sigma_start=0.2,
+        sigma_min=0.05,
+        alpha=0.1,
+    )
+    error_log = "./../data/error_log_pushT/cem_{seed}.npy".format(seed=seed)
+    
 elif args.algorithm == "mtp":
     print("Running MTP")
     ctrl = MTP(
-            task,
-            num_samples=128,
-            M=2, # horizon via control points
-            N=16, # samples 
-            sigma_min=0.1,
-            sigma_start=0.2,
-            num_elites=10,
-            beta=0.25,
-            alpha=0.1,
-            interpolation='bspline',
-            num_randomizations=5,
-            seed=seed,
-        )
+        task,
+        num_samples=128,
+        M=3, # horizon via control points
+        N=32, # samples 
+        sigma_min=0.1,
+        sigma_start=0.2,
+        num_elites=12,
+        beta=0.35,
+        alpha=0.1,
+        interpolation='bspline',
+        num_randomizations=1,
+        seed=seed,
+    )
+    error_log = "./../data/error_log_pushT/mtp_{seed}.npy".format(seed=seed)
+    
 elif args.algorithm == "anmtp":
     print("Running AnMTP")
     ctrl = AnMTP(
             task,
-            num_samples=64,
-            M=2, # horizon via control points
-            N=16, # samples 
+            num_samples=128,
+            M=3, # horizon via control points
+            N=32, # samples 
             sigma_min=0.05,
-            sigma_start=0.1,
+            sigma_start=0.2,
             num_elites=12,
-            keep_elites=2,   # !experimental
-            beta = 0.3,
+            keep_elites=4,   # !experimental
+            beta = 0.25,
             beta_lr = 0.1,        # adaptation step size
             beta_min = 0.05,
             beta_max = 0.35,
             alpha=0.05,
             interpolation='bspline',
-            num_randomizations=8,
+            num_randomizations=1,
             seed=seed,
         )
+    error_log = "./../data/error_log_pushT/anmtp_{seed}.npy".format(seed=seed)
+    
 # Define the model used for simulation
 mj_model, mj_data = task.reset(seed=seed)
 
 # Run the interactive simulation
 
-# run_interactive(
-#     ctrl,
-#     mj_model,
-#     mj_data,
-#     frequency=25,
-#     show_traces=True,
-#     trace_width=0.55,
-#     max_traces=16,
-#     fixed_camera_id=0,
-#     show_ui=True,
-#     record_video=False,
-#     seed=seed,
-#     )
-
-run_headless_simulation(
-    task,
+run_interactive(
     ctrl,
-    frequency=50,
-    seeds=[seed],
-    max_step=1000,
-    log_file_prefix="pusht_franka_" + args.algorithm,
-    save_path="./results"
+    mj_model,
+    mj_data,
+    frequency=25,
+    show_traces=True,
+    trace_width=0.55,
+    max_traces=16,
+    fixed_camera_id=0,
+    show_ui=True,
+    record_video=False,
+    max_step=500,
+    seed=seed,
+    error_log_path=error_log,
     )
+
+
+# run_headless_simulation(
+#     task,
+#     ctrl,
+#     frequency=50,
+#     seeds=[seed],
+#     max_step=500,
+#     log_file_prefix="pusht_franka_" + args.algorithm,
+#     save_path="./results"
+#     )
